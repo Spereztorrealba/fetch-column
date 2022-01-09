093a3cfa74fdc936d30088969008530c03fa8631
Spereztorrealba <77576941+Spereztorrealba@users.noreply.github.com>
2022-01-08 21:57:25 -0600

    function.js
    function.js

---
+++ function.js
@@ -0,0 +1,17 @@
+const run: glide.Column = async (url, query) => {
+    if (url.value === undefined) {
+        return undefined;
+    }
+    let data = await cache.fetch(url.value.toString());
+    if (query.value !== undefined) {
+        data = jq.json(data, query.value);
+    }
+
+    if (data === null) {
+        return undefined;
+    }
+    if (typeof data === "object" || Array.isArray(data)) {
+        return JSON.stringify(data);
+    }
+    return data;
+};
